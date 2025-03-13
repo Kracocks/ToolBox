10dea74de0f3bd00634ff9b2b9a16af733ede6a2
RomainVoivenel <voivenelromain@gmail.com>
2025-03-13 18:30:32 +0100

    new database

--- data/creation.sql
+++ data/creation.sql
@@ -1,36 +1,21 @@
 pragma foreign_keys = true;
 
-create table if not exists LOGIN(
-    login_id integer primary key autoincrement,
-    email text not null,
-    password text not null
-);
-
 create table if not exists SERVICE(
     service_id integer primary key autoincrement,
-    name text unique not null
+    name text unique not null,
+    url text
 );
 
-create table if not exists USE(
-    login_id integer not null,
-    email text not null,
-    service_id int not null,
-    foreign key (login_id) references LOGIN(login_id) on delete cascade,
-    foreign key (service_id) references SERVICE(service_id) on delete cascade,
-    unique (email, service_id)
-);
-
-create table if not exists TOKEN_GROUP(
-    token_group_id integer primary key autoincrement,
-    login_id int not null ,
-    group_name text not null,
-    foreign key (login_id) references LOGIN(login_id) on delete cascade
+create table if not exists LOGIN (
+     login_id integer primary key,
+     email text,
+     password text,
+     service_id int references SERVICE(service_id) on delete cascade
 );
 
 create table if not exists TOKEN(
     token_id integer primary key autoincrement,
-    token_group_id int not null,
+    login_id int references LOGIN(login_id) on delete cascade,
     value text not null,
-    foreign key (token_group_id) references TOKEN_GROUP(token_group_id) on delete cascade,
-    unique (token_group_id, value)
+    expired_at timestamp
 );
